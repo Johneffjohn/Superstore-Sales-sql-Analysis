109110456bf71b9d24840931645ed03105787660
John Effiong <johnjohneffiong8@gmail.com>
2026-06-10 21:49:16 -0700

    SQL_Queries.sql

---
+++ SQL_Queries.sql
@@ -0,0 +1,30 @@
+SQL_Queries.sql
+SELECT
+ROUND(SUM(Sales),2) AS TotalSales,
+ROUND(SUM(Profit),2) AS TotalProfit,
+SUM(Quantity) AS TotalQuantitySold
+FROM superstoredata;
+SELECT 
+Region,
+SUM(Sales) AS TotalSales
+FROM superstoredata
+GROUP BY Region;
+SELECT 
+Category,
+SUM(Profit) AS TotalProfit
+FROM superstoredata
+GROUP BY Category;
+SELECT 
+Category,
+`Sub-Category`,
+SUM(Sales) AS TotalSales,
+SUM(Profit) AS TotalProfit
+FROM superstoredata
+GROUP BY Category, `Sub-Category`
+HAVING SUM(Profit) < 0;
+SELECT 
+Discount,
+SUM(Sales) AS TotalSales,
+SUM(Profit) AS TotalProfit
+FROM superstoredata
+GROUP BY Discount;
